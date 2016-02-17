ce352734303574afbf109f01a90843c9e644c0fc
Viktor Scheglov <viktor@scheglov.pro>
2016-02-17 11:24:26 +0300

    add more plugins

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 ## Boilerplate gulp system on es2015
+
+
+### Installation
+
+```sh
+$ npm install gulpjs/gulp#4.0
+
+$ npm install
+```
+
+### MUST HAVE plugin:
+
+ - 
+ - 
+ - 
+ - 
--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,51 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	concat = require('gulp-concat'),
 	csso = require('gulp-csso'),
-	debug = require('gulp-debug');
+	gulpIf = require('gulp-if'),
+	newer = require('gulp-newer'),
+	size = require('gulp-size'),
+	autoprefixer = require('gulp-autoprefixer'),
+	imagemin = require('gulp-imagemin');
+	del = require('del'),
 
+	debug = require('gulp-debug');
+// Сборка стилей
 gulp.task('style', function() {
 	return gulp.src('dev/style/**/*.scss')
-			.pipe(debug())
+			.pipe(size())
+			.pipe(autoprefixer())
+			.pipe(sourcemaps.init())
 	 		.pipe(sass().on('error', sass.logError))
-			.pipe(sourcemaps.write())
 	    	.pipe(concat('all.css'))
 	    	.pipe(csso())
+	    	.pipe(size())
+	    	.pipe(sourcemaps.write('.'))
 	    	.pipe(debug())
 			.pipe(gulp.dest('public/css'))
-})
+});
+
+gulp.task('clean', function() {
+	return del('public');
+});
+
+gulp.task('assets', function() {
+	return gulp.src('dev/assets/**')
+			.pipe(newer('public'))
+			.pipe(imagemin())
+			.pipe(gulp.dest('public'));
+});
+
+
+gulp.task('build', gulp.series(
+	'clean', 
+	gulp.parallel('style', 'assets')
+	)
+);
+
+gulp.task('watch', function() {
+	gulp.watch('dev/style/**/*.*', gulp.series('style'));
+	gulp.watch('dev/assets/**/*.*', gulp.series('assets'));
+});
+
+
+gulp.task('dev', gulp.series('build', 'watch'));
